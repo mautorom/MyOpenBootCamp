d280ba9c3deb88d2f3d0f8852368e7badd49184a
mauriciotoro-bitbucket@bitbucket.org <mauricio.toro@clave.cl>
2022-12-27 19:01:45 -0300

    Ejercicio Tema 8 Encapsulacion

---
+++ PrivacidadEncapsulacion/src/ejercicioTema8/Main.java
@@ -0,0 +1,62 @@
+package ejercicioTema8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Persona persona = new Persona();
+		
+		persona.setNombre("Mauricio");
+		persona.setEdad(40);
+		persona.setTelefono(12345678);
+		
+		System.out.println("Los datos de persona son");
+		System.out.println("Nombre: " + persona.getNombre());
+		System.out.println("Edad: " + persona.getEdad());
+		System.out.println("Telefono: " + persona.getTelefono());
+		
+	}
+
+}
+
+class Persona{
+	
+	private String nombre;
+	private int edad;
+	private int telefono;
+	
+	public Persona() {
+	}
+	
+	public Persona(String nombre, int edad, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+
+}
